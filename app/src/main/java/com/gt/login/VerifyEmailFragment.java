2cf22914fc5e7d937db922c2e9eae7ee4cf891d1
Groza Teofil <groza_teofil15@yahoo.com>
2023-02-17 14:51:13 +0200

    TeGr 17.02.2023 Changed the code zone

--- app/src/main/java/com/gt/login/VerifyEmailFragment.java
+++ app/src/main/java/com/gt/login/VerifyEmailFragment.java
@@ -1,19 +1,18 @@
 package com.gt.login;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.fragment.app.Fragment;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class VerifyEmailFragment extends Fragment implements View.OnClickListener {
 
     private EditText digit1;
@@ -26,106 +25,70 @@ public class VerifyEmailFragment extends Fragment implements View.OnClickListene
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_verify_email, container, false);
+        View view = inflater.inflate(R.layout.fragment_verify_email, container, false);
 
-        digit1=view.findViewById(R.id.digit1);
-        digit2=view.findViewById(R.id.digit2);
-        digit3=view.findViewById(R.id.digit3);
-        digit4=view.findViewById(R.id.digit4);
+        digit1 = view.findViewById(R.id.digit1);
+        digit2 = view.findViewById(R.id.digit2);
+        digit3 = view.findViewById(R.id.digit3);
+        digit4 = view.findViewById(R.id.digit4);
 
-        resendCodeButton=view.findViewById(R.id.resendCodeButton);
-        verifyButton=view.findViewById(R.id.verifyButton);
+        resendCodeButton = view.findViewById(R.id.resendCodeButton);
+        verifyButton = view.findViewById(R.id.verifyButton);
 
         resendCodeButton.setOnClickListener(this);
         verifyButton.setOnClickListener(this);
 
-        digit1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        nextEditText(digit1, digit2);
+        nextEditText(digit2, digit3);
+        nextEditText(digit3, digit4);
 
-            }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-                if(editable.length()==1){
-                    digit2.requestFocus();
-                }
-
-            }
-        });
-
-        digit2.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-                if(editable.length()==1){
-                    digit3.requestFocus();
-                }
-
-            }
-        });
-
-        digit3.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-                if(editable.length()==1){
-                    digit4.requestFocus();
-                }
-
-            }
-        });
-
-        digit4.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
+        previousEditText(digit1, null);
+        previousEditText(digit2, digit1);
+        previousEditText(digit3, digit2);
+        previousEditText(digit4, digit3);
 
         return view;
     }
 
+    private void nextEditText(EditText currentEditText, EditText nextEditText) {
+        currentEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() == 1) {
+                    nextEditText.requestFocus();
+                }
+            }
+        });
+
+    }
+
+    private void previousEditText(EditText currentEditText, EditText previousEditText) {
+        currentEditText.setOnKeyListener((View view, int i, KeyEvent keyEvent) -> {
+
+            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && keyEvent.getKeyCode() == KeyEvent.KEYCODE_DEL && currentEditText.getText().toString().isEmpty()) {
+                previousEditText.requestFocus();
+                return true;
+
+            }
+            return false;
+
+        });
+    }
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.resendCodeButton:
                 break;
 
@@ -135,5 +98,5 @@ public class VerifyEmailFragment extends Fragment implements View.OnClickListene
         }
 
     }
-    
+
 }
